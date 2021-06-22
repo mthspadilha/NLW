b71886d6a22745a1e988578b73899a777a92433c
Matheus Padilha <matheuspadilha.m@gmail.com>
2021-06-22 17:13:37 -0300

    typeORM add

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,17 @@ import express, { request, response } from "express";
  * DELETE => Remover uma informação
  * PATH => Alterar um informação especifica. ex. alterar a senha de um usuário
  */
-
-
-
+/** TIPOS DE PARAMETRO
+ * Route params => http://localhost:8080/produtos/901283902183
+ * Query params => http://localhost:8080/produto?name=teclado&descripton=mecanico
+ *  NÃO OBRIGATÓRIO
+ *  NÃO VEM DESCRITO NA PRÓPRIA ROTA
+ * Body params => {
+ *  "name" : "teclado",
+ *  "description" : "mecânico"
+ * }
+ *  USADO NO CORPO DA REQUISIÇÃO
+ */
 
 //@types/express
 const app = express();
