f5e69b05b4060a8403f0cf200005028d3711c64b
Matheus Padilha <matheuspadilha.m@gmail.com>
2021-06-22 23:41:12 -0300

    ../UserRepository.ts

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,7 +1,26 @@
-import { Entity } from "typeorm";
-
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid"
 @Entity("users")
 class User {
+
+    @PrimaryColumn()
+    readonly id: string; //Somente leitura, apenas inserção pela entidade
+    @Column()
+    name: string;
+    @Column()
+    email: string;
+    @Column()
+    admin: boolean;
+    @CreateDateColumn()
+    created_at: Date;
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
 
 }
 
